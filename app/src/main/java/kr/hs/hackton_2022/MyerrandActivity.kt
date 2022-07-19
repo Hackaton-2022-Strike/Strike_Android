7eb8ad75d278ea2358c13c5bd4de41138225bf6f
bhindor <82446373+bhindor@users.noreply.github.com>
2022-07-19 17:14:29 +0900

    commit

--- app/src/main/java/kr/hs/hackton_2022/MyerrandActivity.kt
+++ app/src/main/java/kr/hs/hackton_2022/MyerrandActivity.kt
@@ -2,10 +2,24 @@ package kr.hs.hackton_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kr.hs.hackton_2022.data.ErrecycleDataItem
+import kr.hs.hackton_2022.databinding.ActivityMyerrandBinding
+import kr.hs.hackton_2022.main.ErRecyclerViewAdapter
 
 class MyerrandActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMyerrandBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_myerrand)
+        binding = ActivityMyerrandBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initDataRecyclerView()
+    }
+
+    fun initDataRecyclerView() {
+        val adapter = ErRecyclerViewAdapter(this)
+        binding.errandRecycle.adapter = adapter
+        binding.errandRecycle.layoutManager = LinearLayoutManager(this)
     }
 }
