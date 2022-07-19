bb583c883de1a7028fd14df5948976e201e913e8
bhindor <82446373+bhindor@users.noreply.github.com>
2022-07-19 19:16:29 +0900

    splash 구현중

---
+++ app/src/main/java/kr/hs/hackton_2022/splash/SplashActivity.kt
@@ -0,0 +1,20 @@
+package kr.hs.hackton_2022.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kr.hs.hackton_2022.LoginActivity
+import kr.hs.hackton_2022.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
